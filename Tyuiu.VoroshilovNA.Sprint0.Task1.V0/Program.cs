8da86cf15fab74c6d94b4230e7949e5adba67c3f
Tyuiu.VoroshilovNA.@outlook.com <voroshilov.5@mail.ru>
2023-09-15 22:18:44 +0500

    Task1

---
+++ Tyuiu.VoroshilovNA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VoroshilovNA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            String name = "Никита";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
